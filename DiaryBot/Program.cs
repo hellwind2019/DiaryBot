182d6113a8ac6e6f25b84414186fcd987b8c457b
hellwind2019 <99964119+hellwind2019@users.noreply.github.com>
2023-03-13 18:15:44 +0200

    first commit

---
+++ DiaryBot/Program.cs
@@ -0,0 +1,38 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+
+var client = new TelegramBotClient("5856433122:AAFbKRNQ3Sb1FKUxTYCOVBB-AwCSlcwLCcE");
+client.StartReceiving(Update, Error);
+
+
+Console.ReadLine();
+
+static Task Error(ITelegramBotClient arg1, Exception arg2, CancellationToken agr3)
+{
+    throw new NotImplementedException();
+}
+
+async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+{
+    var message = update.Message;
+    if (message.Text != null)
+    {
+        Console.WriteLine($"{message.Chat.FirstName}     |   {message.Text}   ");
+        if (message.Text.ToLower().Contains("hello"))
+        {
+           await botClient.SendTextMessageAsync(message.Chat.Id, "Здоровее видали");
+           return;
+        }
+    }
+    if (message.Photo != null)
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Лучше документом");
+        return;
+    }
+    if (message.Photo != null)
+    {
+        await botClient.SendTextMessageAsync(message.Chat.Id, "Ща погодь, сделаю лучше");
+        return;
+    }
+}
