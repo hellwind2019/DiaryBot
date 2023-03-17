fdfe08dca8422a26a36584da155b942adcbbe15f
HellW1nd <daniltorianik2016@gmail.com>
2023-03-17 19:32:02 +0200

    Добавил метод для перевода числа в строку чисел-эмодзи, для отображение "Дня с ботом"

--- DiaryBot/Utils.cs
+++ DiaryBot/Utils.cs
@@ -130,16 +130,57 @@ public class Utils
     {
         const string daysWithBotField = "daysWithBot";
         string date = DateTime.Now.ToShortDateString();
-        return $"{date}   (#{GetUserField(userID,daysWithBotField).Result})\n \n" +
+        string emodjiDay = GetNumberEmoji(int.Parse(GetUserField(userID, daysWithBotField).Result));
+        return $"{date}   #️⃣{emodjiDay}\n \n" +
                $"{postText}" +
                $"\n \n✅Написано с помощью DiaryBot🤖";
     }
 
-    // public static string GetNumberEmoji(int number)
-    // {
-    //     switch (number)
-    //     {
-    //         case 1
-    //     }
-    // }
+    public static string GetNumberEmoji(int number)
+    {
+        var numsArray = SplitToDigits(number);
+        var outputString = "";
+        for (int i = 0; i < numsArray.Length; i++)
+        {
+            switch (numsArray[i])
+            {
+                case 1 : outputString += "1️⃣"; break;
+                case 2 : outputString += "2️⃣"; break;
+                case 3 : outputString += "3️⃣"; break;
+                case 4 : outputString += "4️⃣"; break;
+                case 5 : outputString += "5️⃣"; break;
+                case 6 : outputString += "6️⃣"; break;
+                case 7 : outputString += "7️⃣"; break; 
+                case 8 : outputString += "8️⃣"; break; 
+                case 9 : outputString += "9️⃣"; break;
+                case 0 : outputString += "0️⃣"; break;
+            }
+        }
+
+        return outputString;
+    }
+    /// <summary>
+    /// Разбивка числа на цифры
+    /// </summary>
+    /// <param name="number">Исходное число</param>
+    /// <returns>Массив однозначных чисел</returns>
+    static int[] SplitToDigits(int number)
+    {
+        number = Math.Abs(number);
+        //Если число меньше 10
+        if (number < 10)
+        {
+            return new int[] { number };
+        }
+        //Результирующий массив размером в количество цифр в числе
+        var result = new int[(int)Math.Log10(number) + 1];
+        for (int i = 0; i < result.Length; i++)
+        {
+            //Последняя цифра числа как остаток от деления на 10
+            result[result.Length - i - 1] = number % 10;
+            //уменьшаем исходное число в 10 раз
+            number /= 10;
+        }
+        return result;
+    }
 }
